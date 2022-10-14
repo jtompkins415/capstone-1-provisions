0eab63fa444191c8b6e7af5a15500b9fcd16c5a8
jtompkins415 <john.tompkins415@gmail.com>
2022-10-13 20:36:49 -0700

    create root route & home route

--- app.py
+++ app.py
@@ -10,3 +10,14 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+@app.route('/', methods=['GET'])
+def redirect_home():
+    '''Redirect root route to homepage'''
+
+    return redirect('/provisions/home')
+
+@app.route('/provisions/home', methods=['GET'])
+def show_home_page():
+    '''Render landing page'''
+
+    return render_template('home.html')
