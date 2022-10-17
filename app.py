afa4ce24fbd6da96a9750fbf083bfb96a3087457
jtompkins415 <john.tompkins415@gmail.com>
2022-10-17 13:18:21 -0700

    import models and db connection

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, redirect, flash, session, request, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User, Beer, Brewery, Wine, Winery
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "prOvisions123"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///provisions_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "provisions415"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-
-
 debug = DebugToolbarExtension(app)
+
+connect_db(app)
 
 @app.route('/', methods=['GET'])
 def redirect_home():
