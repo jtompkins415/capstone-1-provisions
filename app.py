6bd2eac8c4605d37767156cbd2443510f222c167
jtompkins415 <john.tompkins415@gmail.com>
2022-10-20 09:38:45 -0700

    create seed command for adding fake user data

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ debug = DebugToolbarExtension(app)
 
 connect_db(app)
 
+
 @app.route('/', methods=['GET'])
 def redirect_home():
     '''Redirect root route to homepage'''
@@ -29,6 +30,21 @@ def show_home_page():
     return render_template('home.html')
 
 ##USER ROUTE
+
+@app.cli.command('seed')
+def seed():
+    db.drop_all()
+    db.create_all()
+    rachel = User(username="rachelgurl235", email="rachel.adams916@gmail.com", first_name="Rachel", last_name="Adams", password="password1", user_city="Sacramento", user_state="CA")
+    doug = User(username="superdrinker34", email="douggydogg@gmail.com", first_name="Doug", last_name="Jones", password="password2", user_city="Los Angeles", user_state= "CA")
+    felix = User(username="felixthecat233", email="felixthecat@yahoo.com", first_name="Felix", last_name="Bower", password="password3", user_city="New Orleans", user_state="LA")
+    gina = User(username="winelover98", email="gina.rochelle40@aol.com", first_name="Gina", last_name="Harrison", password="password4", user_city="Salt Lake City", user_state="UT")
+    morgan = User(username="runandchug32", email="morgan.dunnigan@hotmail.com", first_name="Morgan", last_name="Dunnigan", password="password5", user_city="Burmingham", user_state="AL")
+    lewis = User(username="lewisbiggulp60", email="lewis.killigan@gmail.com", first_name="Lewis", last_name="Killigan", password="password6", user_city="Seattle", user_state="WA")
+
+    db.session.add_all([rachel, doug, felix, gina, morgan, lewis])
+    db.session.commit()
+    print('SEED COMPLETE!')
 
 @app.route('/provisions/user/new', methods=['GET', 'POST'])
 def show_user_form():
