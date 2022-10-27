13c63b2b6cc1319ec45ad2bbd026f88d3c8ffd9b
jtompkins415 <john.tompkins415@gmail.com>
2022-10-27 09:35:15 -0700

    loosen the strict validation of password in form

--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ class UserForm(FlaskForm):
     '''Form for user creation and submition'''
 
     username = StringField('Username', validators=[InputRequired()])
-    password = PasswordField('Password', validators=[Length(min=8)])
+    password = PasswordField('Password', validators=[Length(min=1)])
     email = StringField('Email', validators=[InputRequired()])
     first_name = StringField('First Name', validators=[InputRequired()])
     last_name = StringField('Last Name', validators=[InputRequired()])
