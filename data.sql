eeff08c46e5081b63a3194c7deb8414a58b61d1a
jtompkins415 <john.tompkins415@gmail.com>
2022-11-07 15:14:49 -0800

    create seed data for beer table in db

---
+++ data.sql
@@ -0,0 +1,11 @@
+\c provisions-db;
+
+INSERT INTO beers (beer_name, brewery, style, abv, price, description)
+VALUES 
+('Delirium Tremens', 'Brouwerij Huyghe', 'Belgian Golden Ale', 8.99, 8.5, 'Belgian fruit and spice notes, light gold color, smooth drinking for a higher abv beer, voted world`s best beer'),
+('Pliny the Elder', 'Russian River', 'DIPA', 8.00,8.5, 'The cult classic from Russian River! Solid malt backbone supports fresh hop flavors'),
+('Saison Dupont','Brasserie Dupont', 'Belgian Farmhouse Ale', 6.99, 6.5, 'Coppery blond, the finest aromas and a strong bitterness transform this beer into a thirst-quenchener with no equal, just the way it was created. Dupont`s selection of yeasts is the perfect base for these typical aromas and ditto taste. A real refermentation in the bottle, which will continue for a long time in your cellar, result into this complex and particular aromatic beer.'),
+('10 Degrees', 'Urban Roots Brewing','Czech Style Lager',5.99,3.9, 'Czech Style served in a Czech style beer glass and from a traditional “side pull” tap handle for a truly authentic experience. This beer is brewed with 100% Czech grown Saaz hops and floor malted Bohemian Pilsner malt made just outside of Prague (thanks to Weyermann Specialty Malts) which lends it plenty of richness and complexity with a classic hop flavor and aroma'),
+('Righteo', 'Original Pattern', 'Hazy Pale Ale', 6.49, 5.6, 'NZ Rakau, NZ Moutere, and Mosaic hops create flavorful notes of apricot, grapefruit, and pine that pair beautifully with a medium, pillowy body, and low-to-no hop bitterness.'),
+('Orval', 'Brasserie d`Orval', 'Trappist Ale', 8.00, 6.9,' Orval is brewed unfiltered by Catholic monks at a monastery founded in the 1100s in the pastoral Belgium countryside. Three different malts and two types of hops, & Brettanomyces yeast, impart a great character and complexity.');
+
