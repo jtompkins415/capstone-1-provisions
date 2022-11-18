961b4c937c34e57a324c1989e4a3b13a4f9829c4
jtompkins415 <john.tompkins415@gmail.com>
2022-11-17 19:33:30 -0800

    Create User edit form

--- forms.py
+++ forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, PasswordField, TextAreaField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, Email
 
 class UserForm(FlaskForm):
     '''Form for user creation and submition'''
 
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[Length(min=1)])
-    email = StringField('Email', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email()])
     first_name = StringField('First Name', validators=[InputRequired()])
     last_name = StringField('Last Name', validators=[InputRequired()])
     user_city = StringField('City')
@@ -29,4 +29,18 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired(),Length(min=1)])
+
+class UserEditForm(FlaskForm):
+    '''Form for editing user information'''
+
+    username = StringField('New Username')
+    email = StringField('New Email', validators=[Email()])
+    first_name = StringField('New First Name')
+    last_name = StringField('New Last Name')
+    user_city = StringField('New City')
+    user_state = StringField('New State')
+
+    
+
+
 
